233a45b2c2b4b42094cd1c574e01c6fb1c737da3
creepereye1204 <112455232+creepereye1204@users.noreply.github.com>
2024-05-14 09:13:02 +0900

    Update utils.py

--- middleware/utils.py
+++ middleware/utils.py
@@ -5,14 +5,14 @@ from uuid import uuid4
 import os
 IMAGE_DIR = os.path.join("resource", "image")
 
-# uuid4를 사용하여 무작위 UUID 생성
+
 def change_file_name(image):
     unique_filename = f"{uuid4()}.{image.filename.split('.')[-1]}"
 
-    # 저장할 경로 설정
+   
     save_path = os.path.join("resource", "image", unique_filename)
 
-    # 이미지 파일 저장
+    
     with open(save_path, "wb") as buffer:
         buffer.write(image.file.read())
 
